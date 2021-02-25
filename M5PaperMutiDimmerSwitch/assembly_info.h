2189fccf07dc784cb15df9154caf956b263d45a0
Ry0 <k104037r@mail.kyutech.jp>
2021-02-26 01:39:39 +0900

    [Add] 定数を別ファイルに設置

---
+++ M5PaperMutiDimmerSwitch/assembly_info.h
@@ -0,0 +1,10 @@
+#ifndef _ASSEMBLY_INFO_H_
+#define _ASSEMBLY_INFO_H_
+
+const char *user_setting_model_name = "M5Paper Muti Dimmer Switch";
+const char *user_setting_manufacturer = "Ryo Kabutan";
+const char *user_setting_serial_number = "0";
+const char *user_setting_models_type = "EspHapButton";
+const char *user_setting_version = "1.0";
+
+#endif
---
+++ M5PaperMutiDimmerSwitch/wifi_info.h
@@ -0,0 +1,7 @@
+#ifndef _WIFI_INFO_H_
+#define _WIFI_INFO_H_
+
+const char *ssid = "ssid";
+const char *password = "password";
+
+#endif
